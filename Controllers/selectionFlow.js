c9bbfeb12ea63006a01a2bec02176240c597e5e7
Zachary Cole <clackary@gmail.com>
2017-03-29 22:48:53 -0600

    added course and instructor controllers, also added some comments/documenation and other gardening

--- Controllers/selectionFlow.js
+++ Controllers/selectionFlow.js
@@ -1,5 +1,9 @@
 var app = angular.module("EvalSystem", []);
 
+/* -- TermController --
+  A term refers to a combination of Year + Semester in the format "2017 Spring"
+  It is generic, and is not dependent on any prior selections.
+*/
 app.controller("TermController", function($scope, $http) {
 	$http(
 		{
@@ -25,11 +29,73 @@ app.controller("TermController", function($scope, $http) {
 }
 });
 
+/* -- Department Controller --
+  This controller returns a list of department names only which have instructors
+  It is generic, and is not dependent on any prior selections.
+*/
 app.controller("DeptController", function($scope, $http) {
 	$http(
 		{
 			method: 'GET',
 			url: 'https://icarus.cs.weber.edu/~nb06777/CS4450/v1/departments'
+		}
+	)
+  /*
+  .then(response => {
+    console.log(response.data);
+    return response.data;
+  }) */
+	.then(
+		function successCallback(response) {
+			$scope.depts = response.data;
+		},
+		function errorCallback(response) {
+			console.log(response);
+		}
+	);
+	$scope.editFunction = function() {
+		console.log("Edit clicked.");
+}
+});
+
+/* -- Course Controller --
+  TODO Needs endpoint and corresponding URL for get method
+  Depends on previously select Term and Department, possibly Instructor
+*/
+app.controller("CourseController", function($scope, $http) {
+	$http(
+		{
+			method: 'GET',
+			//url: 'https://icarus.cs.weber.edu/~nb06777/CS4450/v1/departments'
+		}
+	)
+  /*
+  .then(response => {
+    console.log(response.data);
+    return response.data;
+  }) */
+	.then(
+		function successCallback(response) {
+			$scope.depts = response.data;
+		},
+		function errorCallback(response) {
+			console.log(response);
+		}
+	);
+	$scope.editFunction = function() {
+		console.log("Edit clicked.");
+}
+});
+
+/* -- Instructor Controller --
+  TODO Needs endpoint and corresponding URL for get method
+  Depends on previously select Term and Department, possibly Course
+*/
+app.controller("InstructorController", function($scope, $http) {
+	$http(
+		{
+			method: 'GET',
+			//url: 'https://icarus.cs.weber.edu/~nb06777/CS4450/v1/departments'
 		}
 	)
   /*
