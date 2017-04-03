aa90485ba93118f5cb2c5f44a29bc64bc443b3eb
nateonguitar@gmail.com <!Bella22>
2017-04-03 11:22:41 -0600

    making selection flow work

--- Controllers/selectionFlow.js
+++ Controllers/selectionFlow.js
@@ -1,4 +1,7 @@
 var app = angular.module("EvalSystem", []);
+var termsSelected = [];
+var departmentsSelected = [];
+var courseOrInstructor = "";
 
 /* -- TermController --
   A term refers to a combination of Year + Semester in the format "2017 Spring"
@@ -26,8 +29,59 @@ app.controller("TermController", function($scope, $http) {
 	);
 	$scope.editFunction = function() {
 		console.log("Edit clicked.");
-}
+	}
 });
+
+function buildTermsSelected(){
+	var terms = document.getElementById("termList").options;
+	termsSelected = [];
+	
+	// strip out the information we need to get objects in the form of
+	// {year: 2017, semester: 2}
+	for(var i=0; i<terms.length; i++){
+		if(terms[i].selected){
+			termsSelected.push({
+					Year: terms[i].text.substr(0, 4),
+					SemesterID: terms[i].value
+				});
+		}
+	}
+	
+	console.log(termsSelected);
+	document.getElementById('pane2-button').click();
+}
+
+function buildDepartmentsSelected(){
+	var departments = document.getElementById("deptList").options;
+	departmentsSelected = [];
+	
+	// strip out the information we need to get objects in the form of
+	// {year: 2017, semester: 2}
+	for(var i=0; i<departments.length; i++){
+		if(departments[i].selected){
+			departmentsSelected.push({
+					DepartmentCode: departments[i].value,
+					DepartmentName: departments[i].text
+				});
+		}
+	}
+	
+	console.log(departmentsSelected);
+	document.getElementById('pane3-button').click();
+}
+
+function setCourseOrInstructor(){
+	var radioButtonList = document.getElementsByName("sortBy-Radio");
+	for(let i=0; i<radioButtonList.length; i++){
+		if(radioButtonList[i].checked){
+			courseOrInstructor = radioButtonList[i].value;
+			break;
+		}
+	}
+
+	document.getElementById('pane4-button').click();
+}
+
 
 /* -- Department Controller --
   This controller returns a list of department names only which have instructors
@@ -96,6 +150,7 @@ app.controller("CourseController", function($scope, $http) {
   selections.
 */
 app.controller("InstructorController", function($scope, $http) {
+	
 	$http(
 		{
 			method: 'GET',
@@ -121,25 +176,68 @@ app.controller("InstructorController", function($scope, $http) {
 });
 
 app.controller("InstructorCourseController", function($scope, $http) {
-    $http(
-        {
-            method: 'GET',
-            //url: 'https://icarus.cs.weber.edu/~nb06777/CS4450/v1/departments'
-        }
-    )
-	/*
-	 .then(response => {
-	 console.log(response.data);
-	 return response.data;
-	 }) */
-        .then(
-            function successCallback(response) {
-                $scope.depts = response.data;
-            },
-            function errorCallback(response) {
-                console.log(response);
-            }
-        );
+	//TODO: this isn't hooking up with the front endpoint
+	// $scope.setCourseOrInstructorAngular should fire off when Next is hit
+	// when choosing Course or Instructor radio buttons
+	
+	// setCourseOrInstructor() sets the global variable courseOrInstructor
+	// as whatever radio button was selected and makes the next accordion piece come forward
+	$scope.setCourseOrInstructorAngular = function(){
+		setCourseOrInstructor();
+		if(courseOrInstructor == "Instructor"){
+			$http(
+				{
+					method: 'GET',
+					//url: 'https://icarus.cs.weber.edu/~nb06777/CS4450/v1/departments'
+				}
+			)
+			/*
+			 .then(response => {
+			 console.log(response.data);
+			 return response.data;
+			 }) */
+				.then(
+					function successCallback(response) {
+						$scope.depts = response.data;
+					},
+					function errorCallback(response) {
+						console.log(response);
+					}
+				);
+		}
+		else if(courseOrInstructor == "Course"){
+				let departmentsSelectedJSONString = "";
+				for(let i=0; i<departmentsSelected.length; i++){
+					departmentsSelectedJSONString += "{" +
+						"\"DepartmentCode\" : \"" + departmentsSelected[i].DepartmentCode + "\"," +
+						"\"DepartmentName\" : \"" + departmentsSelected[i].DepartmentName + "\"" +
+					"},";
+				}
+				$http(
+				{
+					method: 'POST',
+					data: departmentsSelectedJSONString,
+					url: 'https://icarus.cs.weber.edu/~nb06777/CS4450/v1/courses'
+				}
+			)
+			/*
+			 .then(response => {
+			 console.log(response.data);
+			 return response.data;
+			 }) */
+			.then(
+				function successCallback(response) {
+					console.log(response.data);
+					$scope.courses = response.data;
+				},
+				function errorCallback(response) {
+					console.log(response);
+				}
+			);
+		}
+	}
+	
+    
     $scope.editFunction = function() {
         console.log("Edit clicked.");
     }
