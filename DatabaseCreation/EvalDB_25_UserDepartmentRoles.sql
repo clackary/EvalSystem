f3ac9308ebce0856d9ee36d90985cf616281d81d
kylerichetti <kyle.richetti@gmail.com>
2017-04-19 10:28:43 -0600

    Database and Readme updates

---
+++ DatabaseCreation/EvalDB_25_UserDepartmentRoles.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE Evals_UserDepartmentRoles(
+	ID INT IDENTITY(1,1) NOT NULL,
+	userID INT NOT NULL,
+	departmentCode INT NOT NULL,
+	roleID INT NOT NULL
+)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (990224681, 6003, 12)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (990224681, 7001, 3)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (887969243, 8001, 21)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (885025795, 8001, 13)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (886227766, 8001, 12)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (885025795, 8001, 3)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (885025795, 8001, 11)
+
+INSERT INTO Evals_UserDepartmentRoles (userID, departmentCode, roleID)
+VALUES (887969243, 8001, 3)
